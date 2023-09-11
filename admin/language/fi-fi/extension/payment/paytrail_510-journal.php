bf07f47ef5cc9f40175e6e63c6e6e0897db683f6
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-09-11 09:46:21 +0300

    Create paytrail_510-journal.php

---
+++ admin/language/fi-fi/extension/payment/paytrail_510-journal.php
@@ -0,0 +1,81 @@
+<?php
+// Heading
+$_['heading_title']          = 'Paytrail 5.10 Journal';
+
+// Text 
+$_['text_payment']           = 'Maksutavat';
+$_['text_success']           = 'Maksutavan Paytrail 5.10 muokkaaminen suoritettu!';
+$_['text_paytrail_510_journal']      = '<a target="_BLANK" href="https://www.paytrail.com/"><img src="view/image/payment/paytrail.png" alt="Paytrail  verkkosivusto" title="Paytrail verkkosivusto" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_clear_success']     = 'Lokitietojen tyhjennys onnistui!';
+$_['text_edit']              = 'Muokkaa Paytrailia';
+$_['text_extension']         = 'Laajennukset';
+      
+// Entry
+$_['entry_merchant']            = 'Myyjän tunniste';
+$_['entry_security']            = 'Turva-avain';
+$_['entry_order_status']        = 'Tilauksen tila';
+$_['entry_order_cancel_status'] = 'Epäonnistuneen tilauksen tila:';
+$_['entry_geo_zone']            = 'Maantieteellinen alue:';
+$_['entry_status']              = 'Tila';
+$_['entry_sandbox']             = 'Testitila';
+$_['entry_log']                 = 'Loki';
+$_['entry_failed_log']          = 'Loki 2';
+$_['entry_store']               = 'Kauppa';
+$_['entry_button_background']   = 'Maksu-painikkeen taustaväri';
+
+// Banks
+$_['entry_general']              = 'General';
+$_['entry_aktia']               = 'Aktia';
+$_['entry_alandsbanken']        = 'Ålandsbanken';
+$_['entry_danskebank']          = 'Danskebank';
+$_['entry_handelsbank']         = 'Handelsbank';
+$_['entry_mastercard']          = 'Mastercard';
+$_['entry_nordea']              = 'Nordea';
+$_['entry_omasp']               = 'OmaSp';
+$_['entry_osuuspankki']         = 'Osuuspankki';
+$_['entry_pivo']                = 'Pivo';
+$_['entry_pop_pankki']          = 'POP Pankki';
+$_['entry_spankki']             = 'S-Pankki';
+$_['entry_saastopankki']        = 'Säästöpankki';
+$_['entry_visa']                = 'Visa';
+$_['entry_visa_electron']       = 'Visa Electron';
+$_['entry_walley']              = 'Walley';
+$_['entry_walley_yrityslasku']  = 'Walley Yrityslasku';
+$_['entry_aktia_sort_order']               = 'Aktia Järjetys';
+$_['entry_alandsbanken_sort_order']        = 'AÅlandsbanken Järjetys';
+$_['entry_danskebank_sort_order']          = 'Danskebank Järjetys';
+$_['entry_handelsbank_sort_order']         = 'Handelsbank Järjetys';
+$_['entry_mastercard_sort_order']          = 'Mastercard Järjetys';
+$_['entry_nordea_sort_order']              = 'Nordea Järjetys';
+$_['entry_omasp_sort_order']               = 'Oma Sp Järjetys';
+$_['entry_osuuspankki_sort_order']         = 'Osuuspankki Järjetys';
+$_['entry_pivo_sort_order']                = 'Pivo Järjetys';
+$_['entry_pop_pankki_sort_order']          = 'POP Pankki Järjetys';
+$_['entry_sapnkki_sort_order']             = 'S-Pankki Järjetys';
+$_['entry_saastopankki_sort_order']        = 'Säästöpankki Järjetys';
+$_['entry_visa_sort_order']                = 'Visa Järjetys';
+$_['entry_visa_electron_sort_order']       = 'Visa Electron Järjetys';
+$_['entry_walley_sort_order']              = 'Walley Järjetys';
+$_['entry_walley_yrityslasku_sort_order']  = 'Walley Yrityslasku Järjetys';
+
+// Placeholder
+$_['placeholder_merchant']      = 'Aseta Kauppiastunnus';
+$_['placeholder_security']      = 'Aseta Salainen avain';
+
+// Error
+$_['error_permission']   = 'Varoitus: Sinulla ei ole oikeuksia maksutavan Paytrail muokkaamiseen!';
+$_['error_merchant']     = 'Myyjän tunniste (MERCHANT) on pakollinen tieto!';
+$_['error_security']     = 'Turva-avain on pakollinen tieto!';
+
+// Help
+$_['help_merchant']      = 'Myyjän tunniste eli Merchant ID, jonka olet saanut Paytraililta.';
+$_['help_security']      = 'Turva-avain eli Secret Key, jonka olet saanut Paytraililta.';
+$_['help_order_status']  = 'Onnistuneen tilauksen tila, kun asiakas palaa maksamisen jälkeen pankin sivulta kauppaan.';
+$_['help_cancel_status'] = 'Epäonnistuneen tilauksen tila, kun asiakas palaa pankin sivulta kauppaan (joko peruutettuaan itse maksamisen tai maksaminen on muusta syystä keskeytynyt).';
+$_['help_geo_zone']      = 'Valitse tähän se alue, jonka alueella verkkokaupassasi on myyntiä.';
+$_['help_sandbox']       = 'Voit tällä testata tämän laajennuksen toimintoja. Kun käytät tätä testitilaa, \'Myyjän tunniste\' ja \'Turva-avain\' eivät ole pakollisia tietoja.';
+$_['help_font_size']     = 'Tekstin \'Verkkokauppa\' koko pikseleissä (syötä vain numeroita!)';
+$_['help_log']           = 'Loki-tiedosto, joka sisätää kaiken liikenteen, onnistuneet ja epäonnistuneet maksutapahtumat.';
+$_['help_failed_log']    = 'Loki-tiedoston, joka sisätää jostakin syystä epäonnistuneet tai asiakkaan omatoimisesti peruuttamat maksutapahtumat.';
+$_['help_button']        = 'Kassalla näkyvän \'Siirry maksmaan\' painikkeen tasutaväri.';
+?>
