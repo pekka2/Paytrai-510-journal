78c2ecfe377e6b57d57754c4961cb2d93d51ad15
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-09-11 10:13:39 +0300

    Add files via upload

---
+++ catalog/controller/extension/payment/pop.php
@@ -0,0 +1,22 @@
+<?php
+class ControllerExtensionPaymentPop extends Controller {
+  
+	public function index() {
+
+        $this->load->language('extension/payment/paytrail_510_journal');
+
+         $data['postcode'] = '';
+
+    $this->load->model('checkout/order');
+        
+    if(isset($this->session->data['order_id'])){
+        $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
+
+        if($order_info['payment_postcode'] && $order_info['payment_lastname'] && $order_info['shipping_postcode']) {
+           $data['postcode'] = $order_info['payment_postcode'];
+        }
+     }
+		return $this->load->view('extension/payment/paytrail_510_journal', $data);
+	}
+}
+?>
---
+++ catalog/controller/extension/payment/saastopankki.php
@@ -0,0 +1,25 @@
+<?php
+class ControllerExtensionPaymentSaastopankki extends Controller {
+
+	public function index() {
+    require_once(DIR_SYSTEM . 'library/paytrail_510_journal.php');
+
+		  $this->load->language('extension/payment/paytrail_510_journal');
+
+        $data['postcode'] = '';
+
+    $this->load->model('checkout/order');
+        
+    if(isset($this->session->data['order_id'])){
+        $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
+
+        if($order_info['payment_postcode'] && $order_info['payment_lastname'] && $order_info['shipping_postcode']) {
+           $data['postcode'] = $order_info['payment_postcode'];
+        }
+     }
+         
+		  return $this->load->view('extension/payment/paytrail_510_journal', $data);
+	}
+
+}
+?>
