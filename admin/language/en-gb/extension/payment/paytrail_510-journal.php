003ef3fc2f706ab119655764ce5720bc3ebb3089
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-09-11 09:49:03 +0300

    Create paytrail_510-journal.php

---
+++ admin/language/en-gb/extension/payment/paytrail_510-journal.php
@@ -0,0 +1,82 @@
+<?php
+// Heading
+$_['heading_title']          = 'Paytrail 5.10 Journal';
+
+// Text 
+$_['text_edit']              = 'Edit Paytrail';
+$_['text_payment']           = 'Payment method';
+$_['text_success']           = 'Paytrail payment method modification has been successfull!';
+$_['text_paytrail_510_journal']     = '<a target="_BLANK" href="https://www.paytrail.com/"><img src="view/image/payment/paytrail.png" alt="Paytrail Website" title="Paytrail Website" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_clear_success']     = 'Erasing of log files has been successfull!';
+$_['text_extension']         = 'Extensions';
+      
+// Entry
+$_['entry_merchant']            = 'Merchant identification';
+$_['entry_security']            = 'Secret key';
+$_['entry_order_status']        = 'Order status';
+$_['entry_order_cancel_status'] = 'Cancelled order status:';
+$_['entry_geo_zone']            = 'Geo zone:';
+$_['entry_status']              = 'Status';
+$_['entry_sandbox']             = 'Testmode';
+$_['entry_log']                 = 'Log';
+$_['entry_failed_log']          = 'Log 2';
+$_['entry_store']               = 'Store';
+$_['entry_button_background']   = 'Maksu-painikkeen taustaväri';
+
+// Banks
+$_['entry_general']              = 'General';
+
+$_['entry_aktia']               = 'Aktia';
+$_['entry_alandsbanken']        = 'Ålandsbanken';
+$_['entry_danskebank']          = 'Danskebank';
+$_['entry_handelsbank']         = 'Handelsbank';
+$_['entry_mastercard']          = 'Mastercard';
+$_['entry_nordea']              = 'Nordea';
+$_['entry_omasp']               = 'OmaSp';
+$_['entry_osuuspankki']         = 'Osuuspankki';
+$_['entry_pivo']                = 'Pivo';
+$_['entry_pop_pankki']          = 'POP Bank';
+$_['entry_spankki']             = 'S-Bank';
+$_['entry_saastopankki']        = 'Säästöpankki';
+$_['entry_visa']                = 'Visa';
+$_['entry_visa_electron']       = 'Visa Electron';
+$_['entry_walley']              = 'Walley';
+$_['entry_walley_yrityslasku']  = 'Walley Yrityslasku';
+$_['entry_aktia_sort_order']               = 'Aktia Sort Order';
+$_['entry_alandsbanken_sort_order']        = 'AÅlandsbanken Sort Order';
+$_['entry_danskebank_sort_order']          = 'Danskebank Sort Order';
+$_['entry_handelsbank_sort_order']         = 'Handelsbank Sort Order';
+$_['entry_mastercard_sort_order']          = 'Mastercard Sort Order';
+$_['entry_nordea_sort_order']              = 'Nordea Sort Order';
+$_['entry_omasp_sort_order']               = 'Oma Sp Sort Order';
+$_['entry_osuuspankki_sort_order']         = 'Osuuspankki Sort Order';
+$_['entry_pivo_sort_order']                = 'Pivo Sort Order';
+$_['entry_pop_pankki_sort_order']          = 'POP Pankki Sort Order';
+$_['entry_sapnkki_sort_order']             = 'S-Pankki Sort Order';
+$_['entry_saastopankki_sort_order']        = 'Säästöpankki Sort Order';
+$_['entry_visa_sort_order']                = 'Visa Sort Order';
+$_['entry_visa_electron_sort_order']       = 'Visa Electron Sort Order';
+$_['entry_walley_sort_order']              = 'Walley Sort Order';
+$_['entry_walley_yrityslasku_sort_order']  = 'Walley Yrityslasku Sort Order';
+
+// Placeholder
+$_['placeholder_merchant']      = 'Set Merchant ID';
+$_['placeholder_security']      = 'Set the Secret Key';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have the rights to modify Paytrail 5.10 payment method!';
+$_['error_merchant']     = 'Merchant identification is a required information!';
+$_['error_security']     = 'Secret key is a required information!';
+
+// Help
+$_['help_merchant']      = 'Merchant identification ie merchant ID, which has been provided to you by Paytrail.';
+$_['help_security']      = 'Secret Key, which has been provided to you by Paytrail 5.10.';
+$_['help_order_status']  = 'Successful order status, when client returns back to store after payment from the bank page.';
+$_['help_cancel_status'] = 'Failed order status, when client returns back to store after payment from the bank page (either cancels payment himself or payment has been cancelled for some other reason).';
+$_['help_geo_zone']      = 'Select the geo zone here, where your web shop has sales.';
+$_['help_sandbox']       = 'You can test this extension functionality. When you use testmode \'Merchant identification\' and \'Secret key\' are not required information.';
+$_['help_font_size']     = 'Text \'Web Payment\' size in pixels (insert just numbers!)';
+$_['help_log']          = 'Log file, which contains all traffic from successful and failed payment transactions.';
+$_['help_failed_log']   = 'Log file, which contain payment tranactions which had failed from some reason or payment transactions which had been independly cancelled by the customer.';
+$_['help_button']        = 'Kassalla näkyvän \'Siirry maksmaan\' painikkeen tasutaväri.';
+?>
