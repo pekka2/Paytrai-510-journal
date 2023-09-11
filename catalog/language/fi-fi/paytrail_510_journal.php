d853016187bffd697b4e9959fa69af5899b3e4c3
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-09-11 10:28:41 +0300

    Create paytrail_510_journal.php

---
+++ catalog/language/fi-fi/paytrail_510_journal.php
@@ -0,0 +1,33 @@
+<?php
+// Text
+$_['text_nordea']            = 'Nordea';
+$_['text_osuuspankki']       = 'Osuuspankki';
+$_['text_pivo']              = 'Pivo';
+$_['text_saastopankki']      = 'Säästöpankki';
+$_['text_omasp']             = 'Oma SP';
+$_['text_aktia']             = 'Aktia';
+$_['text_spankki']           = 'S-Pankki';
+$_['text_pop_pankki']        = 'Pop Pankki';
+$_['text_handelsbanken']     = 'Handelsbanken';
+$_['text_danskebank']        = 'Danskebank';
+$_['text_alandsbanken']     = 'Ålansbanken';
+$_['text_visa']              = 'Visa';
+$_['text_visa_electron']     = 'Visa Electron';
+$_['text_mastercard']        = 'Mastercard';
+$_['text_walley']            = 'Walley';
+$_['text_walley_yrityslasku'] = 'Walley Yrityslasku';
+$_['text_paid_success']     = 'Maksu on suoritettu onnistuneesti! Maksajan pankki: ';
+$_['text_paid_cancel']      = 'Maksun suorittaminen on keskeytetty';
+$_['text_information']      = 'Tietoja käsitellään .... ';
+$_['text_success']          = 'Maksutapa on valittu onnistuneesti!';
+
+// Button
+$_['button_pay']            = 'SIIRRY MAKSAMAAN';
+
+// Help
+$_['help_banks']            = 'Voit maksaa suoraan oman verkkopankkisi kautta';
+$_['help_credit']           = 'Voit maksaa valitsemallasi pankki- tai luottokortilla';
+
+// Error
+$_['error_order_totals']     = '<b>VIRHE!</b> Verkkokaupan laskema tuotteiden ja muiden kulujen yhteishinta kassalla on pienempi kuin ostoksen sekä toimitus- ja muiden kulujen todellinen yhteishinta!';
+?>
